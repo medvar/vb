5e90d2ed7f46ed250971b22dc866efe16ad0d5ae
medvar <30646788+medvar@users.noreply.github.com>
2019-07-22 21:46:29 -0600

    opti
    2

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,7 +1,12 @@
 ﻿Public Class Form1
     Private Sub BnIngresar_Click(sender As Object, e As EventArgs) Handles BnIngresar.Click
+
         Dim usuario As String
         Dim contrase1, contrase2, contrase3 As String
+        Dim msg1, tipo As String
+        msg1 = ""
+        tipo = ""
+
         contrase1 = "CED"
         contrase2 = "Progra2019"
         contrase3 = "2002"
@@ -11,36 +16,19 @@
         usuario = TxbUsuario.Text
 
         If LCase(contrase1) = ContraseIngresada Then
-            Dim msg1 As String = "Bienvenido " + usuario
-
-            MessageBox.Show(UCase(msg1),
-                            "BIENVENIDO",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Exclamation)
-
+            msg1 = "Bienvenido " + usuario
         ElseIf LCase(contrase2) = ContraseIngresada Then
-            Dim msg1 As String = "Bienvenido " + usuario
-
-            MessageBox.Show(UCase(msg1),
-                            "BIENVENIDO ",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Exclamation)
-
+            msg1 = "Bienvenido " + usuario
         ElseIf LCase(contrase3) = ContraseIngresada Then
-            Dim msg1 As String = "Bienvenido " + usuario
-
-            MessageBox.Show(UCase(msg1),
-                            "BIENVENIDO",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Exclamation)
-
+            msg1 = "Bienvenido " + usuario
         Else
-            Dim msg1 As String = "Contraseña incorrecta"
-
-            MessageBox.Show(UCase(msg1),
-                            "ERROR",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Exclamation)
+            msg1 = "Contraseña incorrecta"
+            tipo = "ERROR"
         End If
+
+        MessageBox.Show(UCase(msg1),
+                           tipo,
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Exclamation)
     End Sub
 End Class
